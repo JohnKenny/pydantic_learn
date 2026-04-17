24f9b844e65989d1734d40eb7091e908868c280d
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-17 13:28:38 +0100

    fields, Optional & multiple type added to class

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 """Pydantic basics"""
 
 from pydantic import BaseModel
+from typing import Optional
 
 class User(BaseModel):
     name: str
     age: int
+    induction_passed: bool
+    #years_service: Optional[int] = 0
+    years_service: int | float | str
 
-user = User(name="Juan", age=32)
+
+
+user = User(name="Gemma", age="30", induction_passed=True, years_service="zero")
 
 print(user)
