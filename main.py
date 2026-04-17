aed992186aff72ac155003e2455f30ac134c4cb7
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-17 16:05:44 +0100

    added field validators

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ class Person(BaseModel):
     name: str
     age: int
 
-juan = Person(name="Juan", age=23)
+    @field_validator("name")
+    def check_name_is_alpha(n):
+        if not n.isalpha():
+            raise ValueError
+        return n
+    
+    @field_validator("age")
+    def check_correct_age(n):
+        if n < 1 or n > 120:
+            raise ValueError(f"Invalid age field: ... {n}")
+        return n
+
+
+
+
+
+juan = Person(name="Juan", age=35)
 print(juan)
 
-
+    
