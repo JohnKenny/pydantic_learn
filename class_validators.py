c9ddbc7283a428597beb9c45429e42c698c2dbfa
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-18 13:51:21 +0100

    ItemtoShip class and model validation

--- class_validators.py
+++ class_validators.py
@@ -12,5 +12,20 @@ class User(BaseModel):
             raise ValueError("No coincide.")
         return self
 
-user = User(password="abc", confirm_password="abcd")
+user = User(password="abc", confirm_password="abc")
 print(user)
+
+
+class ItemtoShip(BaseModel):
+    name:str
+    shipping_needed: bool
+    shipping_address: Optional[str] = ""
+
+    @model_validator(mode="after")
+    def item_check(self):
+        print (vars(self))
+        if self.shipping_needed and not self.shipping_address:
+            raise ValueError("Direccion de envio no presente.")
+        return self
+
+item = ItemtoShip(name="juguete", shipping_needed=True, shipping_address="1 punto principal")
