7947eead06daa53bb3a9fefe51583d0d240252d3
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-19 20:31:06 +0100

    settings precedence test.

---
+++ export_vars.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+export APP_NAME="my_app"
+export ENVIRONMENT="testing"
+export APP_VERSION="1.1.12"
---
+++ settings_test.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str
+    environment: str
+    app_version: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+print(settings)
