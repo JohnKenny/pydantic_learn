5ba9871a01ef1e4fad6c0ba0209f468b8ddc12f8
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-19 13:36:52 +0100

    pydantic and yaml

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-"""Pydantic basics - Validators"""
+"""Pydantic basics - YAML"""
 
-from pydantic import BaseModel, field_validator
+import yaml 
 
-class Person(BaseModel):
-    name: str
-    lname: str
-    age: int
-
-    # multi-field validator
-    @field_validator("name", "lname")
-    def check_name_is_alpha(n):
-        if not n.isalpha():
-            raise ValueError(f"{n} is incorrectly formed.")
-        return n.title()
-    
-    @field_validator("age")
-    def check_correct_age(n):
-        if n < 1 or n > 120:
-            raise ValueError(f"Invalid age field: ... {n}")
-        return n
+from pydantic import BaseModel, field_validator, model_validator
 
 
-juan = Person(name="juan", lname="gaunt", age=35)
-print(juan)
 
     
