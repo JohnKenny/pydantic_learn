281ae06ab003ccd0cf433ca1d8105d055d3cfe83
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-19 22:49:05 +0100

    field checks added

--- fields_test.py
+++ fields_test.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel, Field
 
 class Person(BaseModel):
-    name: str = Field()
+    name: str = Field(..., min_length=5, max_length=25)
+    age: int = Field(default=31, ge=1, le=120)
+    zipcode: str = Field(..., pattern=r'^\d{5}$')
 
-person = Person(name="Louise")
+person = Person(name="Emma Greene", zipcode="12345")
 print(person)
