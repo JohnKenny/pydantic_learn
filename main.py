e006495ceaad04c564a40b9c3dc6fe42fb913eca
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-19 14:57:56 +0100

    yaml unpacked to object.

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import yaml
 
 from pydantic import BaseModel, field_validator, model_validator
 
+class App(BaseModel):
+    name: str
+    version: float
+
+    
+
+with open("my_config.yaml") as file:
+    contents = yaml.safe_load(file.read())["app"]
+
+print(contents, "\n\n")
+app = App(**contents)
+print(app)
 
 
     
