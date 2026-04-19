a9f1da94b4f228004c02181bc50ac926c4ccc13d
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-19 13:28:52 +0100

    classmethod added to make obj from json

--- parse_person.py
+++ parse_person.py
@@ -1,7 +1,20 @@
 import json
 from pydantic import BaseModel
 
+class Person(BaseModel):
+    name:str
+    location: str
+    age: int
 
-with open("person.json") as file:
-    contents = file.read()
-    print(contents)
+    @classmethod
+    def make_person_from_json(cls, file_path: str) -> Person:
+        with open(file_path) as file:
+            return cls(**json.loads(file.read()))
+        
+person = Person.make_person_from_json("person.json")
+print(person)
+
+# with open("person.json") as file:
+#     contents = json.loads(file.read())
+#     person = Person(**contents)
+#     print(person)
